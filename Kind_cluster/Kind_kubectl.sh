0a20b3d4cc5092022305d5c22226fa4e5dabe317
Tarasankar-create <dastarasankar393@gmail.com>
2026-05-07 21:36:25 +0530

    Add installation script for Kind and kubectl
    Script installs Kind and kubectl for AMD64 architecture.

---
+++ Kind_cluster/Kind_kubectl.sh
@@ -0,0 +1,17 @@
+# For AMD64 / x86_64
+[ $(uname -m) = x86_64 ] && curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.31.0/kind-linux-amd64
+chmod +x ./kind
+sudo cp ./kind usr/local/bin/kind
+
+VERSION="v1.36.0"
+URL="https://dl.k8s.io/release/${Version}/bin/linux/arm64/kubectl"
+INSTALL_DIR="/usr/local/bin"
+
+curl -LO "$URL"
+chmod +x kubectl
+sudo mv kubectl $INSTALL_DIR/
+
+rm -f kubectl
+rm -rf kind
+
+echo "Installation complete"
