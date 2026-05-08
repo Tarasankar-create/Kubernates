e693ec768b72707d05f148d9d35f7fa424b5370a
Tarasankar-create <dastarasankar393@gmail.com>
2026-05-08 22:02:26 +0530

    Refactor Kind and kubectl installation script

--- Kind_cluster/Kind_kubectl.sh
+++ Kind_cluster/Kind_kubectl.sh
@@ -1,19 +1,36 @@
 #!/bin/bash
+set -e
 
-# For AMD64 / x86_64
-[ $(uname -m) = x86_64 ] && curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.31.0/kind-linux-amd64
-chmod +x ./kind
-sudo cp ./kind usr/local/bin/kind
-
-VERSION="v1.36.0"
-URL="https://dl.k8s.io/release/${Version}/bin/linux/arm64/kubectl"
 INSTALL_DIR="/usr/local/bin"
 
-curl -LO "$URL"
+ARCH=$(uname -m)
+
+if [ "$ARCH" = "x86_64" ]; then
+    KIND_URL="https://kind.sigs.k8s.io/dl/v0.31.0/kind-linux-amd64"
+    KUBECTL_ARCH="amd64"
+elif [ "$ARCH" = "aarch64" ] || [ "$ARCH" = "arm64" ]; then
+    KIND_URL="https://kind.sigs.k8s.io/dl/v0.31.0/kind-linux-arm64"
+    KUBECTL_ARCH="arm64"
+else
+    echo "Unsupported architecture: $ARCH"
+    exit 1
+fi
+
+echo "Installing kind..."
+curl -Lo kind "$KIND_URL"
+chmod +x kind
+sudo mv kind $INSTALL_DIR/
+
+VERSION="v1.36.0"
+URL="https://dl.k8s.io/release/${VERSION}/bin/linux/${KUBECTL_ARCH}/kubectl"
+
+echo "Installing kubectl..."
+curl -Lo kubectl "$URL"
 chmod +x kubectl
 sudo mv kubectl $INSTALL_DIR/
 
-rm -f kubectl
-rm -rf kind
+echo "Verifying installation..."
+kind --version
+kubectl version --client
 
 echo "Installation complete"
